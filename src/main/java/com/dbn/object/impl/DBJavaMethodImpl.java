31ab0f08a53d0a5a1c8df2af6e918973369ed90c
Rishabh Agrawal <rishabh.a.agrawal@oracle.com>
2024-11-19 11:12:47 +0530

    JDBC-3606 - Show method overload index

--- src/main/java/com/dbn/object/impl/DBJavaMethodImpl.java
+++ src/main/java/com/dbn/object/impl/DBJavaMethodImpl.java
@@ -1,15 +1,17 @@
 /*
- * Copyright (c) 2024, Oracle and/or its affiliates.
+ * Copyright 2024 Oracle and/or its affiliates
  *
- * This software is dual-licensed to you under the Universal Permissive License
- * (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License
- * 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose
- * either license.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and limitations under the License.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.dbn.object.impl;
@@ -27,11 +29,12 @@ import org.jetbrains.annotations.NotNull;
 
 import java.sql.SQLException;
 
-import static com.dbn.object.common.property.DBObjectProperty.*;
+import static com.dbn.object.common.property.DBObjectProperty.PUBLIC;
+import static com.dbn.object.common.property.DBObjectProperty.STATIC;
 
 @Getter
 public class DBJavaMethodImpl extends DBObjectImpl<DBJavaMethodMetadata> implements DBJavaMethod {
-	//protected short overload; TODO
+	protected short overload;
 	protected short position;
 
 	public DBJavaMethodImpl(@NotNull DBJavaObject javaObject, DBJavaMethodMetadata metadata) throws SQLException {
@@ -51,11 +54,16 @@ public class DBJavaMethodImpl extends DBObjectImpl<DBJavaMethodMetadata> impleme
 	@Override
 	protected String initObject(ConnectionHandler connection, DBObject parentObject, DBJavaMethodMetadata metadata) throws SQLException {
 		position = metadata.getPosition();
+		overload = metadata.getOverload();
 
-		set(HIDDEN,true);
 		set(PUBLIC,metadata.isPublic());
 		set(STATIC,metadata.isStatic());
 		return metadata.getMethodName();
+	}
+
+	@Override
+	public String getPresentableTextDetails() {
+		return overload > 0 ? " #" + overload : "";
 	}
 
 	@Override
