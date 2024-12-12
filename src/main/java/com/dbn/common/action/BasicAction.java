92fdfb40294fdc7970db4c302333807cbbd7e58d
Dan Cioca <dan.cioca@oracle.com>
2024-12-12 16:33:16 +0100

    JDBC-3759 DBN I18n (action stubs - inspection annotations)

--- src/main/java/com/dbn/common/action/BasicAction.java
+++ src/main/java/com/dbn/common/action/BasicAction.java
@@ -22,6 +22,8 @@ import com.dbn.nls.NlsSupport;
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.util.NlsActions.ActionDescription;
+import com.intellij.openapi.util.NlsActions.ActionText;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -36,11 +38,11 @@ public abstract class BasicAction extends AnAction implements BackgroundUpdateAw
         super(icon);
     }
 
-    public BasicAction(@Nullable String text) {
+    public BasicAction(@Nullable @ActionText String text) {
         super(text);
     }
 
-    public BasicAction(@Nullable String text, @Nullable String description, @Nullable Icon icon) {
+    public BasicAction(@Nullable @ActionText String text, @Nullable @ActionDescription String description, @Nullable Icon icon) {
         super(text, description, icon);
     }
 
